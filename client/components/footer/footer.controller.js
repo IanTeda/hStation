e241b3649c38d37c3a6e705ff4d00eee2032928e
Ian Teda <ian@teda.id.au>
2014-08-09 22:12:43 +1000

    Add footer controller

--- client/components/footer/footer.controller.js
+++ client/components/footer/footer.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('hStationApp')
-  .controller('FooterCtrl', function ($scope, $location, Auth) {
+  .controller('FooterCtrl', function ($scope, $http) {
+
+    console.log('FooterCtrl');
+
+    $scope.hStationApp = [
+      {
+        'name': 'Not Found',
+        'description' : 'Description',
+        'author' : 'Some One',
+        'email' : 'someone@hotmail.com',
+        'homepage' : 'www.someone.com',
+        'version': '-1'
+      }
+    ];
+
+    $http.get('/api/app/version').success(function(hStationAppVersion) {
+      $scope.hStationApp.version = hStationAppVersion;
+    });
 
   });
