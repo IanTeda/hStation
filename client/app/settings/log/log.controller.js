656ad8b35b40c5cae90d30de49940ad63c67d05f
Ian Teda <ian@teda.id.au>
2014-08-24 13:10:34 +1000

    Tidy up log controller

--- client/app/settings/log/log.controller.js
+++ client/app/settings/log/log.controller.js
@@ -2,14 +2,10 @@
 
 angular.module('hStationApp')
   .controller('LogCtrl', function ($scope, $http) {
-    console.log('LogCtrl');
-    $scope.message = 'Hello';
 
-    $scope.logs = [];
-
+    // Request JSON from server end point
     $http.get('/api/logs').success(function(logs) {
       $scope.logs = logs;
-      console.log(JSON.stringify(logs));
     });
 
   });
